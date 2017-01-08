43c51cddf964999e171d853026937bfb9cd93aaf
Nam Dinh <nam.dinhviet@gmail.com>
2017-01-08 03:11:43 -0600

    Can now list custom commands

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const jsonFileIO = require('jsonfile');
 const commandListFile = './commandList.json'; 
 const manual = "```css\n" +
   "`help: Display this message\n" +
-  "`add command content: Add a new command\n" +
-  "`del command: Delete a command\n" + 
+  "`add <command> <content>: Add a new command\n" +
+  "`del <command>: Delete a command\n" + 
+  "`list: List all custom commands\n" + 
   "```";
+const announcementID = process.env.ANNOUNCEMENT_ID;
 
 const prefix = '`';
 // Load the command list from disk
 var commandList = jsonFileIO.readFileSync(commandListFile);
 
-client.on('ready', () => console.log("I'm ready!"));
+client.on('ready', () => {
+  console.log("I'm ready!");
+});
 
 client.on('message', msg => {
   // Skip if message is bot
@@ -31,6 +35,9 @@ client.on('message', msg => {
     // Display help message
     if (msg.content.startsWith(prefix + 'help')) 
       msg.channel.sendMessage(manual);
+    // List all custom commands
+    if (msg.content.startsWith(prefix + 'list'))
+      listUpdatedCommands();
     // Add manual commands
     if (msg.content.startsWith(prefix + 'add')) {
       // Slice to peel off command
@@ -38,6 +45,7 @@ client.on('message', msg => {
       let replyMessage = replyArray.join(' ');
       commandList[newCommand] = replyMessage; // Update the list of commands
       msg.channel.sendMessage(`Added ${msg.author.username}'s new command`);
+      listUpdatedCommands();
     }
     // Delete manual commands
     if (msg.content.startsWith(prefix + 'del')) {
@@ -45,6 +53,7 @@ client.on('message', msg => {
       let commandToDel = msg.content.split(' ').slice(1);
       delete commandList[commandToDel];
       msg.channel.sendMessage('Deleted ' + commandToDel);
+      listUpdatedCommands();
     }
     // Added commands functions
     else {
@@ -53,6 +62,32 @@ client.on('message', msg => {
       if (commandList[command]) 
         msg.channel.sendMessage(commandList[command]);
     }
+  }
+
+  function ayylmao(message) {
+    const regex = /(^|\b)ay{2,}/gi; // only at start of sentence or word
+    let lmao = {}; // result to return
+    let result = regex.exec(message); // array of match info
+    if (result === null) {
+      return {
+        valid: false
+      };
+    } 
+    // There's a match here
+    lmao.valid = true;
+    lmao.length = result[0].length - 2; // exclude 'a' and one 'y'
+    return lmao;
+  }
+
+  function listUpdatedCommands() {
+    let message = "Here is the command list:\n```css\n";
+    for (let command in commandList) {
+      // Disregard inherited property from prototype
+      if (commandList.hasOwnProperty(command)) 
+        message += command + ': ' + commandList[command] + '\n';
+    }
+    message += "```";
+    client.channels.get(announcementID).sendMessage(message);
   }
 });
 
@@ -68,20 +103,6 @@ process.on('SIGINT', () => {
 
 client.login(process.env.TOKEN);
 
-function ayylmao(message) {
-  const regex = /(^|\b)ay{2,}/gi; // only at start of sentence or word
-  let lmao = {}; // result to return
-  let result = regex.exec(message); // array of match info
-  if (result === null) {
-    return {
-      valid: false
-    };
-  } 
-  // There's a match here
-  lmao.valid = true;
-  lmao.length = result[0].length - 2; // exclude 'a' and one 'y'
-  return lmao;
-}
 
 function updateCommandList() {
   jsonFileIO.writeFileSync(commandListFile, commandList);
